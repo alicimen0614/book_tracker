48d1b3a2e7be9457c4c76e555c36b1cfd6eed88a
alicimen0614 <alicimen0614@gmail.com>
2024-02-18 01:28:48 +0300

    improvement on shimmer UI on author page

--- lib/screens/discover_screen/shimmer_effect_builders/author_info_body_shimmer.dart
+++ lib/screens/discover_screen/shimmer_effect_builders/author_info_body_shimmer.dart
@@ -17,9 +17,11 @@ Expanded authorInfoBodyShimmerBuilder(BuildContext context) {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Divider(color: Colors.transparent, thickness: 0),
-            ShimmerWidget.rectangular(
-                width: MediaQuery.sizeOf(context).width / 3, height: 10),
-            Divider(color: Colors.transparent, thickness: 0),
+            ShimmerWidget.rounded(
+                width: MediaQuery.sizeOf(context).width / 2,
+                height: 15,
+                shapeBorder: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))),
             Divider(color: Colors.transparent, thickness: 0),
             Text(
               "Biyografi",
@@ -27,58 +29,75 @@ Expanded authorInfoBodyShimmerBuilder(BuildContext context) {
             ),
             Divider(color: Colors.transparent, thickness: 0),
             SizedBox(
-                height: 120,
-                child: ListView.separated(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) => ShimmerWidget.rectangular(
-                          height: 15,
-                          width: MediaQuery.sizeOf(context).width - 40,
-                        ),
-                    separatorBuilder: (context, index) => SizedBox(
-                          height: 5,
-                        ),
-                    itemCount: 5)),
-            Align(
-              alignment: Alignment.centerRight,
-              child: ShimmerWidget.rectangular(
-                  width: MediaQuery.sizeOf(context).width / 3, height: 10),
+              height: MediaQuery.sizeOf(context).height / 5.5,
+              child: ListView.separated(
+                physics: NeverScrollableScrollPhysics(),
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 5,
+                ),
+                itemCount: 5,
+                itemBuilder: (context, index) => ShimmerWidget.rounded(
+                    height: 15,
+                    width: MediaQuery.sizeOf(context).width - 30,
+                    shapeBorder: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15))),
+              ),
             ),
+            Align(
+                alignment: Alignment.centerRight,
+                child: ShimmerWidget.rounded(
+                    width: 100,
+                    height: 15,
+                    shapeBorder: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)))),
             Divider(color: Colors.transparent, thickness: 0),
             Text(
               "Yazara Ait Kitaplar",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Divider(color: Colors.transparent, thickness: 0),
-            SizedBox(
-                height: 120,
-                width: double.infinity,
-                child: ListView.builder(
-                    physics: ClampingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) => Container(
-                          height: 100,
-                          width: 100,
-                          child: InkWell(
-                              child: Column(
-                            children: [
-                              Expanded(
-                                  flex: 10,
-                                  child: ShimmerWidget.rectangular(
-                                      width: 50, height: 70)),
-                              Spacer(),
-                              Expanded(
-                                  flex: 2,
-                                  child: ShimmerWidget.rectangular(
-                                      width: 50, height: 10))
-                            ],
-                          )),
-                        ),
-                    itemCount: 5)),
+            Container(
+              height: 150,
+              child: ListView.separated(
+                  physics: NeverScrollableScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Column(
+                        children: [
+                          ShimmerWidget.rounded(
+                              width: 70,
+                              height: 100,
+                              shapeBorder: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15))),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          ShimmerWidget.rounded(
+                              width: 70,
+                              height: 10,
+                              shapeBorder: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15))),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          ShimmerWidget.rounded(
+                              width: 50,
+                              height: 10,
+                              shapeBorder: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15))),
+                        ],
+                      ),
+                  separatorBuilder: (context, index) => SizedBox(width: 20),
+                  itemCount: 4),
+            ),
             Divider(color: Colors.transparent, thickness: 0),
             Align(
               alignment: Alignment.centerRight,
-              child: ShimmerWidget.rectangular(
-                  width: MediaQuery.sizeOf(context).width / 3, height: 10),
+              child: ShimmerWidget.rounded(
+                  width: MediaQuery.sizeOf(context).width / 3,
+                  height: 15,
+                  shapeBorder: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15))),
             )
           ],
         ),
